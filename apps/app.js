ee4fbc480c84d37d597fc6df19927c099f96fa15
cbilliau <christopher@billiau.com>
2016-05-26 09:40:19 -0400

    Refactored code to seperate functions.

--- apps/app.js
+++ apps/app.js
@@ -1,9 +1,28 @@
 $(function(){
-
-  $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data) {
-    var myData = data.Search;
-    $.each(myData, function(index, value){
-      console.log(value.Title);
-    })
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
   });
 });
+
+function getRequest(searchTerm){
+  var params = {
+    s: searchTerm,
+    r: 'json'
+  };
+  url = 'http://www.omdbapi.com';
+
+  $.getJSON(url, params, function(data){
+    showResults(data.Search);
+  });
+}
+
+function showResults(results) {
+  var html ='';
+  $.each(results, function(index, value)  {
+    html += '<p>' + value.Title + '</p>';
+    console.log(value.Title);
+  });
+  $('#search-results').html(html);
+}
