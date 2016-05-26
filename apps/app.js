ff1292e6c14c93932a312f61a1de228e9ebcc3c7
cbilliau <christopher@billiau.com>
2016-05-26 05:59:37 -0400

    Add iterate over json search.

--- apps/app.js
+++ apps/app.js
@@ -1,6 +1,9 @@
 $(function(){
 
   $.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data) {
-    console.log(data);
+    var myData = data.Search;
+    $.each(myData, function(index, value){
+      console.log(value.Title);
+    })
   });
 });
